9c1b7b72a9e9466b7df8a4109c25ecd0bd28ee86
logdog42 <73185764+logdog42@users.noreply.github.com>
2020-12-18 23:59:08 -0600

    Initial commit

---
+++ UberSubwayOrBusProgram/UberSubwayOrBusProgram/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmUberSubwayOrBus
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.cboTransportationMethod = New System.Windows.Forms.ComboBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblCommuteCostDisplay = New System.Windows.Forms.Label()
+        Me.lblValidinputs = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cboTransportationMethod
+        '
+        Me.cboTransportationMethod.FormattingEnabled = True
+        Me.cboTransportationMethod.Items.AddRange(New Object() {"Uber", "Subway", "Bus"})
+        Me.cboTransportationMethod.Location = New System.Drawing.Point(450, 190)
+        Me.cboTransportationMethod.Name = "cboTransportationMethod"
+        Me.cboTransportationMethod.Size = New System.Drawing.Size(156, 28)
+        Me.cboTransportationMethod.TabIndex = 0
+        Me.cboTransportationMethod.Text = "Type of Transport"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Tahoma", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.lblTitle.Location = New System.Drawing.Point(368, 62)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(317, 34)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Uber, Subway, or Bus"
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Font = New System.Drawing.Font("Tahoma", 8.0!)
+        Me.lblInstructions.Location = New System.Drawing.Point(369, 114)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(319, 57)
+        Me.lblInstructions.TabIndex = 2
+        Me.lblInstructions.Text = "To calculate yearly commute cost select " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "mode of transportation, and enter the " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "requested information. Then click Calculate."
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.Goldenrod
+        Me.btnCalculate.Font = New System.Drawing.Font("Tahoma", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.btnCalculate.Location = New System.Drawing.Point(478, 315)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(100, 49)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.Goldenrod
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 36)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(54, 32)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.ClearToolStripMenuItem.Text = "&Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'lblCommuteCostDisplay
+        '
+        Me.lblCommuteCostDisplay.AutoSize = True
+        Me.lblCommuteCostDisplay.Font = New System.Drawing.Font("Tahoma", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.lblCommuteCostDisplay.Location = New System.Drawing.Point(316, 368)
+        Me.lblCommuteCostDisplay.Name = "lblCommuteCostDisplay"
+        Me.lblCommuteCostDisplay.Size = New System.Drawing.Size(410, 24)
+        Me.lblCommuteCostDisplay.TabIndex = 5
+        Me.lblCommuteCostDisplay.Text = "Your yearly commute cost is, $XXXX.XX"
+        Me.lblCommuteCostDisplay.Visible = False
+        '
+        'lblValidinputs
+        '
+        Me.lblValidinputs.AutoSize = True
+        Me.lblValidinputs.Font = New System.Drawing.Font("Tahoma", 7.5!, System.Drawing.FontStyle.Bold)
+        Me.lblValidinputs.Location = New System.Drawing.Point(345, 263)
+        Me.lblValidinputs.Name = "lblValidinputs"
+        Me.lblValidinputs.Size = New System.Drawing.Size(367, 36)
+        Me.lblValidinputs.TabIndex = 6
+        Me.lblValidinputs.Text = "Acceptable range for Price Per Day: $1 - $59.99" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Acceptable range for Days In a M" &
+    "onth: 1 - 30"
+        '
+        'frmUberSubwayOrBus
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Khaki
+        Me.BackgroundImage = Global.UberSubwayOrBusProgram.My.Resources.Resources.crop3
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(800, 449)
+        Me.Controls.Add(Me.lblValidinputs)
+        Me.Controls.Add(Me.lblCommuteCostDisplay)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.cboTransportationMethod)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmUberSubwayOrBus"
+        Me.Text = "Uber, Subway, or Bus"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cboTransportationMethod As ComboBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents lblCommuteCostDisplay As Label
+    Friend WithEvents lblValidinputs As Label
+End Class
---
+++ UberSubwayOrBusProgram/UberSubwayOrBusProgram/Form1.vb
@@ -0,0 +1,298 @@
+﻿'Program:               Uber, Subway, or Bus 
+'Developer:             Logan Taylor
+'Date:                  12/16/20
+'Purpose:               To calculate yearly commute cost based on user inputs for how they commute.
+
+Option Strict On
+
+Public Class frmUberSubwayOrBus
+
+    'Class Variables
+    Dim decMonthlyCost As Decimal
+    Dim decYearlyCost As Decimal
+    Dim intTransportType As Integer
+    Dim intDaysInMonth As Integer
+    Dim decCostPerDay As Decimal
+    Dim strCostPerDay As String
+    Dim strDailyCost As String
+    Dim strDaysInMonth As String
+    Dim blnValidateDaysInMonth As Boolean = False
+    Dim blnValidateCostPerDay As Boolean = False
+    Dim blnValidateTransportation As Boolean = False
+
+    Private Sub frmUberSubwayOrBus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Executes when application loads
+
+        Threading.Thread.Sleep(5000)
+        lblCommuteCostDisplay.Visible = False
+
+    End Sub
+
+    Private Sub cboTransportationMethod_SelectedIndexChanged(ByVal Sender As System.Object, ByVal e As EventArgs) Handles cboTransportationMethod.SelectedIndexChanged
+        'Allows the user to pick transportation type, then calls sub procedure to display a series of input boxes asking for additional information.
+
+        intTransportType = cboTransportationMethod.SelectedIndex
+        Select Case intTransportType
+
+            Case 0
+
+                UberGroup()
+
+            Case 1
+
+                SubwayGroup()
+
+            Case 2
+
+                BusGroup()
+
+
+        End Select
+
+    End Sub
+
+    Private Sub UberGroup()
+
+        ValidateTransportType()
+    End Sub
+
+    Private Sub BusGroup()
+
+        ValidateTransportType()
+    End Sub
+
+    Private Sub SubwayGroup()
+
+        ValidateTransportType()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' asks for more information, Determines, and displays yearly commuting cost based on user inputs
+
+        'strings used in this sub
+        Dim strDailyCostAsk As String = "How much does it cost for one day of travel?"
+        Dim strCostTitle As String = "Daily Cost"
+        Dim strDaysInMonthAsk As String = "How many days do you travel to work, per month?"
+        Dim strDaysTitle As String = "Days Per Month"
+
+        'executed based on selected transportation type
+        Try
+
+
+            Select Case (cboTransportationMethod.SelectedIndex)
+
+                Case 0
+
+                    If blnValidateTransportation = True Then
+
+                        strDailyCost = InputBox(strDailyCostAsk, strCostTitle)
+                        ValidateCostPerDay()
+
+                        If blnValidateCostPerDay = True Then
+
+                            strDaysInMonth = InputBox(strDaysInMonthAsk, strDaysTitle)
+                            ValidateDaysInMonth()
+
+                            If blnValidateDaysInMonth = True Then
+
+                                decMonthlyCost = Convert.ToDecimal(Convert.ToDecimal(strDailyCost) * 1.2) * intDaysInMonth
+                                decYearlyCost = decMonthlyCost * 12
+                                lblCommuteCostDisplay.Text = "Your yearly commute cost is " & decYearlyCost.ToString("C")
+                                lblCommuteCostDisplay.Visible = True
+                                btnCalculate.Enabled = False
+                            Else
+
+                                MsgBox("Enter a reasonable number of days, please.", , "Wow, did you really?")
+
+                            End If
+
+                        Else
+
+                            MsgBox("Your price has to be between $1 and $59.99", , "Wow, did you really?")
+
+                        End If
+
+                    Else
+
+                        MsgBox("You have to pick a type of transportaion.", , "Wow, did you really?")
+
+                    End If
+
+                Case 1
+
+                    If blnValidateTransportation = True Then
+
+                        strDailyCost = InputBox(strDailyCostAsk, strCostTitle)
+                        ValidateCostPerDay()
+
+                        If blnValidateCostPerDay = True Then
+
+                            strDaysInMonth = InputBox(strDaysInMonthAsk, strDaysTitle)
+                            ValidateDaysInMonth()
+
+                            If blnValidateDaysInMonth = True Then
+
+                                decMonthlyCost = Convert.ToDecimal(Convert.ToDecimal(strDailyCost) * 1) * intDaysInMonth
+                                decYearlyCost = decMonthlyCost * 12
+                                lblCommuteCostDisplay.Text = "Your yearly commute cost is " & decYearlyCost.ToString("C")
+                                lblCommuteCostDisplay.Visible = True
+                                btnCalculate.Enabled = False
+                            Else
+
+                                MsgBox("Enter a reasonable number of days, please.", , "Wow, did you really?")
+
+                            End If
+
+                        Else
+
+                            MsgBox("Your price has to be between $1 and $59.99", , "Wow, did you really?")
+
+                        End If
+
+                    Else
+
+                        MsgBox("You have to pick a type of transportaion.", , "Wow, did you really?")
+
+                    End If
+
+                Case 2
+
+                    If blnValidateTransportation = True Then
+
+                        strDailyCost = InputBox(strDailyCostAsk, strCostTitle)
+                        ValidateCostPerDay()
+
+                        If blnValidateCostPerDay = True Then
+
+                            strDaysInMonth = InputBox(strDaysInMonthAsk, strDaysTitle)
+                            ValidateDaysInMonth()
+
+                            If blnValidateDaysInMonth = True Then
+
+                                decMonthlyCost = Convert.ToDecimal(Convert.ToDecimal(strDailyCost) * 1) * intDaysInMonth
+                                decYearlyCost = decMonthlyCost * 12
+                                lblCommuteCostDisplay.Text = "Your yearly commute cost is " & decYearlyCost.ToString("C")
+                                lblCommuteCostDisplay.Visible = True
+                                btnCalculate.Enabled = False
+
+                            Else
+
+                                MsgBox("Enter a reasonable number of days, please.", , "Wow, did you really?")
+
+                            End If
+
+                        Else
+
+                            MsgBox("Your price has to be between $1 and $59.99", , "Wow, did you really?")
+
+                        End If
+
+                    Else
+
+                        MsgBox("You have to pick a type of transportaion.", , "Wow, did you really?")
+
+                    End If
+
+            End Select
+
+        Catch ex As Exception
+
+            MsgBox("You broke it, Way to go.", , "Wow, did you really?")
+
+
+        End Try
+
+
+
+    End Sub
+
+
+
+
+
+
+
+    Private Function ValidateDaysInMonth() As Boolean
+        'This procedure validates the input for workdays in a month
+
+        intDaysInMonth = Convert.ToInt32(strDaysInMonth)
+
+            If intDaysInMonth < 31 And intDaysInMonth > 0 Then
+
+                blnValidateDaysInMonth = True
+
+            Else
+
+                blnValidateDaysInMonth = False
+                intDaysInMonth = Convert.ToInt32(InputBox("Please enter your average number of workdays in a month; between 1 and 28 days. ", "Error"))
+                ValidateDaysInMonth()
+
+            End If
+
+
+            Return blnValidateDaysInMonth
+
+    End Function
+    Private Function ValidateCostPerDay() As Boolean
+        ' This function validates the Cost Per Day Input
+
+        Dim strDailyCostError As String = "Please enter a valid cost between $1 and $59.99"
+        Dim strMessageBoxTitle As String = "Error"
+
+        decCostPerDay = Convert.ToDecimal(strDailyCost)
+
+            If decCostPerDay > 0.99 And decCostPerDay < 60 Then
+
+                blnValidateCostPerDay = True
+
+            Else
+
+                blnValidateCostPerDay = False
+                decCostPerDay = Convert.ToDecimal(InputBox(strDailyCostError, strMessageBoxTitle))
+                ValidateCostPerDay()
+
+            End If
+
+            Return blnValidateCostPerDay
+
+    End Function
+
+    Private Function ValidateTransportType() As Boolean
+        'Ensures that The type of transportation is selected.
+
+        Dim intTransportType As Integer
+
+        'call a funtion to ensure the type of transport is selected.
+        intTransportType = Convert.ToInt32(cboTransportationMethod.SelectedIndex)
+
+            If cboTransportationMethod.SelectedIndex <= 2 And cboTransportationMethod.SelectedIndex >= 0 Then
+
+                blnValidateTransportation = True
+
+            Else
+                'Detects If a Transport type is Selected
+
+                blnValidateTransportation = False
+                MsgBox("Please select a transport type", , "First Step")
+
+            End If
+
+            Return blnValidateTransportation
+
+    End Function
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+
+        Close()
+
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+        'Executed when menu item Clear is clicked
+
+        lblCommuteCostDisplay.Visible = False
+        cboTransportationMethod.Text = "Type of Transport"
+        btnCalculate.Enabled = True
+
+    End Sub
+End Class
